6639c803b9680809f7adc20d628c99030109d669
stevethomas <steve@codinglabs.com.au>
2024-10-23 22:11:51 +1000

    can skip transcoding step

--- src/Steps/Ensures/EnsureTranscoderExistsStep.php
+++ src/Steps/Ensures/EnsureTranscoderExistsStep.php
@@ -2,8 +2,10 @@
 
 namespace Codinglabs\Yolo\Steps\Ensures;
 
+use Codinglabs\Yolo\Manifest;
 use Codinglabs\Yolo\AwsResources;
 use Codinglabs\Yolo\Contracts\Step;
+use Codinglabs\Yolo\Enums\StepResult;
 use Codinglabs\Yolo\Exceptions\ResourceDoesNotExistException;
 
 class EnsureTranscoderExistsStep implements Step
@@ -11,9 +13,15 @@ class EnsureTranscoderExistsStep implements Step
     /**
      * @throws ResourceDoesNotExistException
      */
-    public function __invoke(): void
+    public function __invoke(): StepResult
     {
+        if (Manifest::get('transcoder') === null) {
+            return StepResult::SKIPPED;
+        }
+
         AwsResources::elasticTranscoderPipeline();
         AwsResources::elasticTranscoderPreset();
+
+        return StepResult::SUCCESS;
     }
 }
