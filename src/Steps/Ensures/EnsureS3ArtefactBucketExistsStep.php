95d0e1fbfd0ca54c9d345efcac32164cbd526a8f
stevethomas <steve@codinglabs.com.au>
2024-10-23 17:17:26 +1000

    fix artefacts bucket name

--- src/Steps/Ensures/EnsureS3ArtefactBucketExistsStep.php
+++ src/Steps/Ensures/EnsureS3ArtefactBucketExistsStep.php
@@ -8,13 +8,12 @@ use Codinglabs\Yolo\Helpers;
 use Codinglabs\Yolo\Manifest;
 use Codinglabs\Yolo\Contracts\Step;
 use Codinglabs\Yolo\Enums\StepResult;
-use function Laravel\Prompts\note;
 
 class EnsureS3ArtefactBucketExistsStep implements Step
 {
     public function __invoke(): StepResult
     {
-        $bucketName = sprintf('%s-%s-yolo-artefacts', Manifest::name(), Helpers::environment());
+        $bucketName = Helpers::keyedResourceName('artefacts');
 
         if (Paths::s3ArtefactsBucket() && Aws::s3()->doesBucketExistV2($bucketName)) {
             return StepResult::SYNCED;
