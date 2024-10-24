ea9784ba28fa3092c898185db36bd26d315456cf
stevethomas <steve@codinglabs.com.au>
2024-10-24 13:06:12 +1000

    implement better checks in command

--- src/Commands/Command.php
+++ src/Commands/Command.php
@@ -7,12 +7,14 @@ use Codinglabs\Yolo\Manifest;
 use Codinglabs\Yolo\Concerns\RegistersAws;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Codinglabs\Yolo\Concerns\ChecksIfCommandsShouldBeRunning;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use function Laravel\Prompts\error;
 
 abstract class Command extends SymfonyCommand
 {
     use RegistersAws;
+    use ChecksIfCommandsShouldBeRunning;
 
     public InputInterface $input;
     public OutputInterface $output;
@@ -22,23 +24,35 @@ abstract class Command extends SymfonyCommand
         Helpers::app()->instance('input', $this->input = $input);
         Helpers::app()->instance('output', $this->output = $output);
 
-        if (! $this->input->hasArgument('environment')) {
-            $this->input->setArgument('environment', 'production');
+        // bail if command should not be running
+        if (! $this->shouldBeRunning($this)) {
+            error(sprintf("Cannot run '%s' in current environment", $this->getName()));
+            return 1;
+        }
+
+        // special handling for `yolo init` command to execute early
+        if ($this instanceof InitCommand) {
+            return (int)(Helpers::app()->call([$this, 'handle']) ?: 0);
+        }
+
+        if (! Manifest::exists()) {
+            error("Could not find yolo.yml manifest in the current directory - run 'yolo init' to create one");
+            return 1;
+        }
+
+        if (! Manifest::environmentExists($this->argument('environment'))) {
+            error(sprintf("Could not find '%s' in the YOLO manifest", $this->argument('environment')));
+            return 1;
+        }
+
+        if (! Helpers::keyedEnv('AWS_PROFILE')) {
+            error(sprintf("You need to specify YOLO_%s_AWS_PROFILE in your .env file before proceeding", strtoupper(Helpers::environment())));
+            return 1;
         }
 
         Helpers::app()->instance('environment', $this->argument('environment'));
 
-        if (Manifest::exists()) {
-            if (! Manifest::environmentExists($this->argument('environment'))) {
-                error("Could not find '{$this->argument('environment')}' in the YOLO manifest");
-                return 1;
-            }
-
-            $this->registerAwsServices();
-        } elseif (! $this instanceof InitCommand) {
-            error("Could not find yolo.yml manifest in the current directory - run 'yolo init' to create one");
-            return 1;
-        }
+        $this->registerAwsServices();
 
         $this->output->setVerbosity(OutputInterface::VERBOSITY_DEBUG);
 
