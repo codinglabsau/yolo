2fca65c8c8a7fc8f65f213d329f0c5d4e37f3038
stevethomas <steve@codinglabs.io>
2024-10-30 17:23:35 +1000

    drop CodeDeploy configuration step

--- src/Commands/SyncCiCommand.php
+++ src/Commands/SyncCiCommand.php
@@ -14,7 +14,7 @@ class SyncCiCommand extends Command
 
     protected array $steps = [
         Steps\Ci\SyncCodeDeployApplicationStep::class,
-        Steps\Ci\SyncCodeDeployDeploymentConfigStep::class,
+//        Steps\Ci\SyncCodeDeployDeploymentConfigStep::class, // todo: drop this once we have an Envoyer style deployment
         Steps\Ci\SyncCodeDeploySchedulerDeploymentGroupStep::class,
         Steps\Ci\SyncCodeDeployQueueDeploymentGroupStep::class,
         Steps\Ci\SyncCodeDeployWebDeploymentGroupStep::class,
