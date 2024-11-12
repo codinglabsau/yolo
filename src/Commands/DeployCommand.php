acb6963c816e3ea831c2a2d93729eeef45ef00cc
stevethomas <steve@codinglabs.io>
2024-11-12 16:14:50 +1000

    update dns last

--- src/Commands/DeployCommand.php
+++ src/Commands/DeployCommand.php
@@ -27,10 +27,10 @@ class DeployCommand extends Command
         Steps\Deploy\CreateArtefactStep::class,
         Steps\Deploy\PushArtefactToS3Step::class,
         Steps\Deploy\PushAssetsToS3Step::class,
-        Steps\Deploy\SyncRecordSetStep::class,
-        Steps\Deploy\SyncMultitenancyRecordSetStep::class,
         Steps\Deploy\UpdateCodeDeployDeploymentGroupStep::class,
         Steps\Deploy\CreateCodeDeployDeploymentsStep::class,
+        Steps\Deploy\SyncRecordSetStep::class,
+        Steps\Deploy\SyncMultitenancyRecordSetStep::class,
         Steps\Build\PurgeBuildStep::class,
     ];
 
