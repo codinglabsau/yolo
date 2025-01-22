ec3492de15b01a21d756cb1f73b406a5265822a3
stevethomas <steve@codinglabs.io>
2025-01-22 16:17:14 +1000

    wip

--- src/Steps/Ensures/EnsureTranscoderExistsStep.php
+++ src/Steps/Ensures/EnsureTranscoderExistsStep.php
@@ -6,21 +6,20 @@ use Codinglabs\Yolo\Manifest;
 use Codinglabs\Yolo\AwsResources;
 use Codinglabs\Yolo\Contracts\Step;
 use Codinglabs\Yolo\Enums\StepResult;
-use Codinglabs\Yolo\Exceptions\ResourceDoesNotExistException;
+use Codinglabs\Yolo\Concerns\EnsuresResourcesExist;
 
 class EnsureTranscoderExistsStep implements Step
 {
-    /**
-     * @throws ResourceDoesNotExistException
-     */
+    use EnsuresResourcesExist;
+
     public function __invoke(): StepResult
     {
         if (Manifest::get('aws.transcoder') === null) {
             return StepResult::SKIPPED;
         }
 
-        AwsResources::elasticTranscoderPipeline();
-        AwsResources::elasticTranscoderPreset();
+        $this->ensure(fn () => AwsResources::elasticTranscoderPipeline());
+        $this->ensure(fn () => AwsResources::elasticTranscoderPreset());
 
         return StepResult::SUCCESS;
     }
