ea22b7e5e0015e7bbf03988c42e63dc320085ef2
stevethomas <steve@codinglabs.io>
2025-02-06 17:24:38 +1000

    check pulse worker in manifest

--- src/Steps/Start/All/SyncPulseWorkerStep.php
+++ src/Steps/Start/All/SyncPulseWorkerStep.php
@@ -12,6 +12,10 @@ class SyncPulseWorkerStep implements RunsOnAws
 {
     public function __invoke(): StepResult
     {
+        if (! Manifest::get('pulse-worker', false)) {
+            return StepResult::SKIPPED;
+        }
+
         file_put_contents(
             sprintf('/etc/supervisor/conf.d/%s', Helpers::keyedResourceName('pulse-worker.conf')),
             str_replace(
