d587dc379507d53b52a2be68870e3c62662d9ca5
stevethomas <steve@codinglabs.io>
2025-02-19 11:16:24 +1000

    wip

--- src/Steps/Deploy/CreateCodeDeployDeploymentsStep.php
+++ src/Steps/Deploy/CreateCodeDeployDeploymentsStep.php
@@ -28,7 +28,6 @@ class CreateCodeDeployDeploymentsStep implements Step
         }
 
         if ($this->shouldRunOnGroup(ServerGroup::QUEUE, $options)) {
-
             $this->createQueueServerDeployment($appVersion);
         }
 
