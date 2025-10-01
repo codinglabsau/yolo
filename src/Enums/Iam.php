3052889cc03915493cf8aa18854a649a2f4cfe11
stevethomas <steve@codinglabs.io>
2025-10-01 16:38:11 +1000

    wip

--- src/Enums/Iam.php
+++ src/Enums/Iam.php
@@ -5,5 +5,5 @@ namespace Codinglabs\Yolo\Enums;
 enum Iam: string
 {
     case INSTANCE_PROFILE = 'instance-profile';
-    case MEDIA_CONVERT_ROLE = 'media-convert-role';
+    case MEDIA_CONVERT_ROLE = 'mediaconvert-role';
 }
